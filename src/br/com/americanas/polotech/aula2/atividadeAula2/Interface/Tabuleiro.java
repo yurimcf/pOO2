a347418694d9df658c7ce3194d25b95247f9614c
yurimcf <yurimcf@gmail.com>
2023-01-27 21:54:58 -0300

    Refatoração do jogo da velha

---
+++ src/br/com/americanas/polotech/aula2/atividadeAula2/Interface/Tabuleiro.java
@@ -0,0 +1,8 @@
+package br.com.americanas.polotech.aula2.atividadeAula2.Interface;
+
+public interface Tabuleiro {
+    void inicialize();
+    void showTabuleiro(char[][] tabuleiro);
+    void tabComJogada(int posicao, char[][] tabuleiro, char character);
+
+}
